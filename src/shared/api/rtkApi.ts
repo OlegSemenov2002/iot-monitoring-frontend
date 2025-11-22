738ccd021b2ccc66e61f03f9319fb100e6f69485
OlegSemenov2002 <534871abv@mail.ru>
2025-11-22 19:03:17 +0300

    vercel fix8

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,9 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
+const getBaseUrl = () => {
+    if (typeof window !== 'undefined') {
+        const host = window.location.hostname;
+        if (host.includes('vercel.app')) {
+            return '/api';
+        }
+    }
+    return __API__;
+};
+
 export const rtkApi = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: __API__,
+        baseUrl: getBaseUrl(),
         prepareHeaders: (headers) => {
             const token = localStorage.getItem(USER_LOCALSTORAGE_KEY);
             if (token) {
